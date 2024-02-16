1253fd26ab299d2907dcfe1c79cf3cb49f1089b0
aco401 <westphal.tobias@gmx.de>
2024-02-16 17:48:07 +0000

    Add SQL Usage

--- src/qt/app/mainwindow.h
+++ src/qt/app/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSqlTableModel>
 #include <QStringList>
 #include <QStringListModel>
 
@@ -14,8 +15,12 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
   public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0, QSqlTableModel *viewModel = nullptr);
     ~MainWindow();
+
+  private:
+    void createMenuBar();
+    void about();
 
   private slots:
     void slotAddTodo();
@@ -23,7 +28,7 @@ class MainWindow : public QMainWindow
 
   private:
     Ui::MainWindow *ui;
-    QStringListModel *model;
+    QSqlTableModel *model;
 };
 
 #endif// MAINWINDOW_H
